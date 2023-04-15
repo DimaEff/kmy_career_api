cbb307e40979b59985c19f7617aff282701a667b
DimaEff <fominienkovd@yandex.ru>
2023-04-15 17:51:00 +0300

    configured the postgres via docker(db data, init script)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val koin_ktor: String by project
-val kmongo_version: String by project
-
 plugins {
     kotlin("jvm") version "1.8.20"
     id("io.ktor.plugin") version "2.2.4"
@@ -28,6 +22,9 @@ repositories {
 }
 
 dependencies {
+    val kotlin_version="1.8.20"
+    val ktor_version="2.2.4"
+    val logback_version="1.2.11"
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
@@ -45,13 +42,24 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
 
     // Koin
+    val koin_ktor="3.4.0"
     implementation("io.insert-koin:koin-ktor:$koin_ktor")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor")
 
-    // kmongo
-    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
-    implementation("org.litote.kmongo:kmongo-id-serialization:$kmongo_version")
+    // db, exposed
+    val exposed_version = "0.41.1"
+    val h2_version = "2.1.214"
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
 
+    // kmongo
+//    val kmongo_version="4.8.0"
+//    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
+//    implementation("org.litote.kmongo:kmongo-id-serialization:$kmongo_version")
+
+    // tests
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
